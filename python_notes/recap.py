14a64c566bd29e5852d72892e1452abe73fbd925
Reyan <dinreyanud@gmail.com>
2022-06-27 17:05:24 +0100

    added python notes folder

---
+++ python_notes/recap.py
@@ -0,0 +1,19 @@
+print("this is my file")
+# = to comment
+
+greeting = "Hello World"
+# = = variable
+
+print(greeting)
+
+# Boolean = True or False
+print(True) # example of Boolean
+print(False) # example of Boolean
+# Interger = Whole number
+print(1234) # example of interger
+# Floating point = Numbers with decimal points
+print(12.34) # example of floating point
+# String = Characters
+print("This is a string for displaying character") # example of string
+# None = Nothing (place holder data)
+print(None) # example of None
