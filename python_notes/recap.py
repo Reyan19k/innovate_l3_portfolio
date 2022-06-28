f105963b017d774dd6fd3d46a11291b87ffd9784
Reyan <dinreyanud@gmail.com>
2022-06-28 12:59:16 +0100

    added python notes folder

--- python_notes/recap.py
+++ python_notes/recap.py
@@ -39,22 +39,22 @@ import random
 
 # print(len("The quick brown fox                         ".strip()))
 
-print(random.random())
-print(random.uniform(1,10))
-print(random.randint(1,10))
+# print(random.random())
+# print(random.uniform(1,10))
+# print(random.randint(1,10))
 
-my_name = "Reyan"
-my_age = 23
-student = False
+# my_name = "Reyan"
+# my_age = 23
+# student = False
 
-print(my_name, my_age, student)
+# print(my_name, my_age, student)
 
-print("My name is", my_name)
-print("I am", my_age, "years old.")
-print("I am a studen:", student)
+# print("My name is", my_name)
+# print("I am", my_age, "years old.")
+# print("I am a studen:", student)
 
-print("Hello my name is {} and I am {} years old".format(my_name,my_age)) # .format method, uses {} as placeholders
-print(f"Hello my name is {my_name} and I am {my_age} years old.") # f string method
+# print("Hello my name is {} and I am {} years old".format(my_name,my_age)) # .format method, uses {} as placeholders
+# print(f"Hello my name is {my_name} and I am {my_age} years old.") # f string method
 
 # # Simple calculation methods
 # print(1+2) # Addition
@@ -74,11 +74,57 @@ print(f"Hello my name is {my_name} and I am {my_age} years old.") # f string met
 # answer = input("What is your name: ")
 # print("Hello,", answer)
 
-music = "classical"
+# music = "classical"
 
-if music == "classical":
-    print("Ah man, not again")
-elif music == "no music":
-    print("Put something on, please")
-else:
-    print("Volume up plaese")
+# if music == "classical":
+#     print("Ah man, not again")
+# elif music == "no music":
+#     print("Put something on, please")
+# else:
+#     print("Volume up plaese")
+
+# print(10%5==0)
+
+# num = 50
+# num2 = 100
+
+# if num > num2:
+#     print(f"{num} is bigger")
+# elif num2 > num:
+#     print(f"{num2} is bigger")
+# else:
+#     print("Bothe are equivalent")
+
+# day = "saturday"
+
+# if day == "saturday" or day == "sunday":
+#     print("The weekend")
+# else:
+#     print("the weekday")
+
+# def light_switch():
+#     print("Lights on")
+
+# light_switch()
+# light_switch()
+
+# def cash_with(amount,accnum):
+#     print(f"You have withdrawn {amount} from {accnum}")
+
+# cash_with(1000,56585485)
+
+fav_games = [
+    "Farcry 3",
+    "The Outerworlds",
+    "Starwars Jedi: Fallen order"
+]
+print(fav_games)
+fav_games [0] = "Farcry 6"
+print(fav_games [0])
+print(fav_games)
+
+fav_games.append("Farcy 3")
+print(fav_games)
+
+fav_games.pop(0)
+print(fav_games)
