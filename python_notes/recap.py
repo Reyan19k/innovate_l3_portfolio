de88947fa51b7fbb1ca263ec36cc936f1de8d681
Reyan <dinreyanud@gmail.com>
2022-06-28 11:56:59 +0100

    added python notes folder

--- python_notes/recap.py
+++ python_notes/recap.py
@@ -1,43 +1,43 @@
 import random
 
-print("this is my file")
-# = to comment
+# print("this is my file")
+# # = to comment
 
-greeting = "Hello World"
-# = = variable
+# greeting = "Hello World"
+# # = = variable
 
-print(greeting)
+# print(greeting)
 
-# Boolean = True or False
-print(True) # example of Boolean
-print(False) # example of Boolean
-# Interger = Whole number
-print(1234) # example of interger
-# Floating point = Numbers with decimal points
-print(12.34) # example of floating point
-# String = Characters
-print("This is a string for displaying character") # example of string
-# None = Nothing (place holder data)
-print(None) # example of None
+# # Boolean = True or False
+# print(True) # example of Boolean
+# print(False) # example of Boolean
+# # Interger = Whole number
+# print(1234) # example of interger
+# # Floating point = Numbers with decimal points
+# print(12.34) # example of floating point
+# # String = Characters
+# print("This is a string for displaying character") # example of string
+# # None = Nothing (place holder data)
+# print(None) # example of None
 
-print(len(greeting))
+# print(len(greeting))
 
-print(greeting[1])
-print(greeting[-1])
+# print(greeting[1])
+# print(greeting[-1])
 
-print(greeting.upper())
+# print(greeting.upper())
 
-print("HELLO".lower())
+# print("HELLO".lower())
 
-print("hello EVERYONE. THIS is innovate".capitalize())
+# print("hello EVERYONE. THIS is innovate".capitalize())
 
-print("The quick brown fox fox fox".count("fox"))
+# print("The quick brown fox fox fox".count("fox"))
 
-print("TheT quick brown fox".find("T"))
+# print("TheT quick brown fox".find("T"))
 
-print("The quick brown fox".replace("fox","frog"))
+# print("The quick brown fox".replace("fox","frog"))
 
-print(len("The quick brown fox                         ".strip()))
+# print(len("The quick brown fox                         ".strip()))
 
 print(random.random())
 print(random.uniform(1,10))
@@ -56,17 +56,29 @@ print("I am a studen:", student)
 print("Hello my name is {} and I am {} years old".format(my_name,my_age)) # .format method, uses {} as placeholders
 print(f"Hello my name is {my_name} and I am {my_age} years old.") # f string method
 
-# Simple calculation methods
-print(1+2) # Addition
-print(6-4) # Subtraction
-print(6*6) # Multiplication
-print(5**5) # to the power of
-print(10/5) # Division
-print(20%2) # Modulas
+# # Simple calculation methods
+# print(1+2) # Addition
+# print(6-4) # Subtraction
+# print(6*6) # Multiplication
+# print(5**5) # to the power of
+# print(10/5) # Division
+# print(20%2) # Modulas
 
-balance = 1000
-print(balance)
-amount = 500
+# balance = 1000
+# print(balance)
+# amount = 500
 
-balance = amount+balance
-print(balance)
+# balance = amount+balance
+# print(balance)
+
+# answer = input("What is your name: ")
+# print("Hello,", answer)
+
+music = "classical"
+
+if music == "classical":
+    print("Ah man, not again")
+elif music == "no music":
+    print("Put something on, please")
+else:
+    print("Volume up plaese")
